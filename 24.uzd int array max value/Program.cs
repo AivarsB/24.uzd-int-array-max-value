b1575790e06c4383d9ddb70ad36486ac6a872ffa
AivarsB <aivars.berzins@inbox.lv>
2020-01-14 15:46:03 +0200

    Add project files.

---
+++ 24.uzd int array max value/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _24.uzd_int_array_max_value
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 2, 3, 14, 25, 66, 77, 88, 2, 3, 4, 9 };
+
+            var maxValue = 0;
+
+            foreach (var number in numbers)
+            {
+                if (number > maxValue)
+                {
+                    maxValue = number;
+                }
+            }
+
+            Console.WriteLine(maxValue);
+        }
+    }
+}
